2848069b93de5c4eebbd9734b2d82f71e6ec480b
Nishant Jannu <nishant@DN51s57n.SUNet>
2022-05-22 23:00:33 -0700

    modified linear dynamics

--- dynamics.py
+++ dynamics.py
@@ -17,7 +17,7 @@ import numpy as np
 m = 1
 
 # Yaw inertial of vehicle
-I_z = 1
+Iz = 1
 
 # Distance to front axis from center of mass
 a = 1
@@ -27,6 +27,25 @@ b = 1
 
 # tire-slip angle to lateral force ratio (for asphalt)
 beta = 1.2 
+
+# linearized version of the fiala tire model 
+# returns force and gradient at given alpha
+def linear_fiala(alpha, road_type):
+    if road_type == "asphalt":
+        if alpha < -6:
+            return 8, 0
+        elif alpha > 6:
+            return -8, 0
+        else:
+            return - 1.33 * alpha, -1.33
+    elif road_type == "ice":
+        if alpha < -1:
+            return 1, 0
+        elif alpha > 1:
+            return -1, 0
+        else:
+            return - alpha, -1.
+
 
 # net tire slip angle in degrees, force in kN
 def calc_lateral_tire_force(angle):
@@ -86,88 +105,41 @@ def no_longitud_dynamics(state, control):
     return 
 
 
-def linear_dynamics(state, control, prev_values):
+def linear_dynamics(prev_values):
 
-    delta = control
-
-    U_y, r, _, _ = state
-
-    ###### TODO: pass kappa ########
-    U_x0, _, kappa = get_path_traj()
-    # Fx_net = - U_y * r * m
+    Ux, kappa = get_path_traj()
     
     # extract previous values
-
-    U_y0, r0, _, _  = prev_values["state"]
-    delta_0 = prev_values["control"]
-    Fy_r0, Fy_f0 = prev_values["tire_forces"]
+    Uyo, ro, _, _  = prev_values["state"]
+    uo = prev_values["control"]
 
     ############ LINEARIZATION ################
 
-    # computing partial derivatives- rear tire forces
-    dFyr_dUy = beta * U_x0 / ((U_y0 - b * r0) * (U_y0 - b * r0)) 
-    dFyr_dr = - b * dFyr_dUy
+    alpha_f_bar = (Uyo + a*ro)/ Ux - uo
+    alpha_r_bar = (Uyo - b*ro)/ Ux
 
-    # linearized rear tire force
-    Fy_r = Fy_r0 + dFyr_dUy * (U_y - U_y0) + dFyr_dr * (r - r0)
-    # Fy_r = A_Fyr @ X + B_Fyr @ U + B_Fyr
-    A_Fyr = np.array([dFyr_dUy, dFyr_dr, 0, 0])
-    B_Fyr = 0
-    C_Fyr = Fy_r0 -  dFyr_dUy *  U_y0 -  dFyr_dr * r0
-    
-    # computing partial derivatives- front tire forces
-    dFyf_dUy = beta * U_x0 / ((U_y0 - a * r0) * (U_y0 - a * r0)) 
-    dFyf_dr = - a * dFyr_dUy
+    fy_f_bar, cf = linear_fiala(alpha_f_bar, "asphalt")
+    fy_r_bar, cr = linear_fiala(alpha_r_bar, "asphalt")
 
-    # linearized front tire force
-    Fy_f = Fy_f0 + dFyf_dUy * (U_y - U_y0) + dFyf_dr * (r - r0) - beta * (delta - delta_0)
+    A_Uy = np.array([(cf + cr)/ (m*Ux), (cf*a - cr*b)/ (m*Ux) - Ux, 0, 0 ])
+    B_Uy = - cf/m
+    C_Uy = (-cf*alpha_f_bar - cr*alpha_r_bar + fy_f_bar + fy_r_bar)/ m
 
-    # Fy_F = A_Fyf @ X + B_Fyf @ U + B_Fyf
-    A_Fyf = np.array([dFyf_dUy, dFyf_dr, 0, 0])
-    B_Fyf = - beta
-    C_Fyf = Fy_f0 -  dFyf_dUy *  U_y0 -  dFyf_dr * r0 + beta * delta_0
+    A_r = np.array([(a*cf - b*cr)/ (Iz*Ux), (cf* a**2 + cr* b**2)/ (Iz*Ux) - Ux, 0, 0 ])
+    B_r = - (a * cf)/Iz
+    C_r = (-a * cf * alpha_f_bar + b * cr * alpha_r_bar + a * fy_f_bar - b * fy_r_bar)/ Iz
 
-    # using linearized tire forces to calculate linearlized dynamics
-    A_Uy = (1/m)*(A_Fyr + A_Fyf) 
-    A_Uy[1] -= U_x0
-    B_Uy = (1/m)*(B_Fyr + B_Fyf)
-    C_Uy = (1/m)*(C_Fyr + C_Fyf)
-
-    A_r = (a * A_Fyr + b * A_Fyf)/ I_z
-    B_r = (a * B_Fyr + b * B_Fyf)/ I_z
-    C_r = (a * C_Fyr + b * C_Fyf)/ I_z
-
-    A_e = np.array([1, 0, 0, U_x0])
+    A_e = np.array([1, 0, 0, Ux])
     B_e = 0
     C_e = 0
 
     A_psi = np.array([0, 1, 0, 0])
     B_psi = 0
-    C_psi = - kappa * U_x0
+    C_psi = - kappa * Ux
 
     A = np.array([A_Uy, A_r, A_e, A_psi])
     B = np.array([B_Uy, B_r, B_e, B_psi])
     C = np.array([C_Uy, C_r, C_e, C_psi])
 
-    ################ DYNAMICS #################
-
-    # U_x_dot = (Fx_net) / m + r* U_y
-    # U_y_dot = (Fy_f + Fy_r) / m - r*U_x
-    # r_dot = (a*Fy_f - b * Fy_r) / I_z
-    # s_dot = U_x - U_y * delta_psi
-    # e_dot = U_x + U_y * delta_psi
-    # delta_psi_dot = r - kappa * s_dot
-
-    ########### INTEGRATE DYNAMICS ##############
-
-    # U_x += U_x_dot * dt 
-    # U_y += U_y_dot * dt
-    # r += r_dot * dt
-    # s += s_dot * dt
-    # e += e_dot * dt
-    # delta_psi += delta_psi_dot * dt
-
-    # next_state = (U_y, r, e, delta_psi)
-    # prev_vals = (U_x, U_y, delta, Fy_r, Fy_f , r)
-
-    return (A, B, C), (Fy_r, Fy_f)
+    return (A, B, C)
+  
